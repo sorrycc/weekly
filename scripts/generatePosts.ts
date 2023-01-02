86297496b9e869c23cd61fb0c556e58f8f72b3f9
sorrycc <sorrycc@gmail.com>
2023-01-02 17:40:27 +0800

    feat: copy posts to public

--- scripts/generatePosts.ts
+++ scripts/generatePosts.ts
@@ -23,3 +23,7 @@ const posts = fs
   .filter(Boolean)
   .sort((a: any, b: any) => (b.number > a.number ? 1 : -1));
 fs.writeFileSync(targetFilePath, JSON.stringify(posts, null, 2), 'utf-8');
+console.log('generate posts.json');
+
+fs.copySync(sourceDir, path.join(__dirname, '../public/posts'));
+console.log('copy posts to public/posts');
