92809ea279a1450eeb8b6fe3e13f5748de057f51
sorrycc <sorrycc@gmail.com>
2023-01-25 17:18:20 +0800

    feat: add favicon

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,7 @@
 export default {
+  favicons: [
+    'https://img.alicdn.com/imgextra/i3/O1CN01uKTVpD1UK8BCxFBwo_!!6000000002498-2-tps-500-500.png',
+  ],
   // why set false?
   // 开启后，会拦截 .json 请求，然后访问 /posts.json 会一直走缓存不生效
   mfsu: false,
