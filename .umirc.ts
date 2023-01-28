037a33f2a8478e433d690407e902d734805dd551
sorrycc <sorrycc@gmail.com>
2023-01-28 18:07:17 +0800

    feat: deploy with hash

--- .umirc.ts
+++ .umirc.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   // why set false?
   // 开启后，会拦截 .json 请求，然后访问 /posts.json 会一直走缓存不生效
   mfsu: false,
+  hash: true,
   chainWebpack(config: any) {
     const REG = /\.md$/;
     config.module.rule('asset').exclude.add(REG).end();
