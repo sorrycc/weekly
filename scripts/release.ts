277b31cab1a5aaa13a08417fdfdbf5679302dc82
sorrycc <sorrycc@gmail.com>
2023-01-29 12:30:38 +0800

    chore: update release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,7 +1,9 @@
 import 'zx/globals';
 import assert from 'assert';
+import getGitRepoInfo from 'git-repo-info';
 
 (async () => {
+  const { branch } = getGitRepoInfo();
   const pkgName = argv.pkg;
   assert(pkgName, `pkg name is required, specify with --pkg=xxx`);
   const pkgPath = path.join(__dirname, '..', pkgName);
@@ -10,9 +12,16 @@ import assert from 'assert';
   await $`cd ${pkgPath} && npm version patch`;
   const newVersion = require(path.join(pkgPath, 'package.json')).version;
   await $`cd ${pkgPath} && npm publish`;
+
+  // modify root pkg
+  const rootPkgPath = path.join(__dirname, '..', 'package.json');
+  const rootPkg = require(rootPkgPath);
+  rootPkg.devDependencies[pkgName] = newVersion;
+  fs.writeFileSync(rootPkgPath, JSON.stringify(rootPkg, null, 2));
+
   await $`git commit -am "release: ${pkgName}@${newVersion}"`;
   await $`git tag ${pkgName}v${newVersion}`;
-  await $`git push --tags`;
+  await $`git push origin ${branch} --tags`;
 })().catch((e) => {
   console.error(e);
   process.exit(1);
