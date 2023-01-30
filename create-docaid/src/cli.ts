48fc20244ba05167d46f0dd997275361d620eb6e
sorrycc <sorrycc@gmail.com>
2023-01-30 22:38:33 +0800

    fix(create-docaid): create dir

--- create-docaid/src/cli.ts
+++ create-docaid/src/cli.ts
@@ -13,7 +13,7 @@ files.forEach((file) => {
   console.log(`Create ${newFile}`);
   const sourceFile = path.join(base, file);
   const targetFile = path.join(cwd, newFile);
-  fs.mkdirSync(path.dirname(sourceFile), { recursive: true });
+  fs.mkdirSync(path.dirname(targetFile), { recursive: true });
   fs.copyFileSync(sourceFile, targetFile);
 });
 console.log('done');
